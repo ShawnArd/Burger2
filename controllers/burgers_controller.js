c1fc5327aeb0e67c86a019419b275f1e4df35ef2
Shawn Ard <shawnduncanard@gmail.com>
2019-01-09 23:20:46 -0600

    attemp at control

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,12 +30,16 @@ router.post("/burgers/create", function(req, res) {
 
 // put route -> back to index
 router.put("/burgers/update/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
+  console.log(req.params.id)
+  var condition = "id = " + req.params.id
+  burger.update("burgers",{
+    devoured:true
+  }, condition, function(res) {  
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
-    console.log(result);
+    
     // Send back response and let page reload from .then in Ajax
-    res.json("/");
+    res.json("/")
   });
 });
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,12 @@
 $(".devour-form").on("submit", function(event) { //on the "devour-form" btn submit
 event.preventDefault();
+//grabs id value from the burger_id class
 var id = $(this).children(".burger_id").val();
 console.log(id)
 $.post("burgers/update/" + id, {
 id: id
-}).then(function(data) {
+}).then(function() {
     // reload page to display devoured burger in proper column
     location.reload();
-    console.log(data)
-})
+    })
 });
